4f16e5801d71cf5f4b165713e222a930524791e5
obflynn <obflynn@bu.edu>
2026-02-16 00:44:24 -0500

    work in progress solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -3,10 +3,29 @@ use crate::strategies::Strategy;
 
 pub struct Part2 {}
 
-// Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
-        // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+
+        let mut next_guess = (min + max)/2;
+        let mut prev_guess = 0;
+        let mut binary_boothang= player.ask_to_compare(next_guess);
+       
+        if binary_boothang == 1 {
+            prev_guess = next_guess;
+            next_guess = (prev_guess + min)/2; 
+        }
+
+        if binary_boothang == -1 {
+            prev_guess = next_guess;
+            next_guess = (prev_guess + max)/2;
+        }
+        
+        if binary_boothang == 0 {
+            return next_guess;
+        }
+
+        else {
+            return 00000; 
+        }
     }
 }
