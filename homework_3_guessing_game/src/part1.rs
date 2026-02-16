7491af87f67f60d310f0b599e13e7d63e9ac742e
obflynn <obflynn@bu.edu>
2026-02-15 23:58:21 -0500

    updated solution for part1

--- homework_3_guessing_game/src/part1.rs
+++ homework_3_guessing_game/src/part1.rs
@@ -3,7 +3,6 @@ use crate::strategies::Strategy;
 
 pub struct Part1 {}
 
-// Terrible strategy: ask if the number is min, otherwise return max.
 impl Strategy for Part1 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         let mut next_guess = min;
