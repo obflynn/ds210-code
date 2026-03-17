bf5073ee81017ac05ec46701de08f8554406ab7e
sarudakbu <sarudak@bu.edu>
2026-03-17 18:54:08 -0400

    Finish v3 implementation (chat history + fixes)

--- project_2_chatbot/basic_chatbot/src/solution/v3.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v3.rs
@@ -3,51 +3,66 @@ use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct ChatbotV3 {
-     model: Llama,
-    sessions: HashMap<String, Chat<Llama>>,
+    model: Llama, 
+    sessions: HashMap<String, Chat<Llama>>, 
 }
+
+impl ChatbotV3 {
+    #[allow(dead_code)]
+    pub fn new(model: Llama) -> ChatbotV3 {
+        return ChatbotV3 {
+            model: model, 
+            sessions: HashMap::new(), // initialize HashMap to store chat session data for each user
+        };
+    }
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-         // creates a new session for a new user
-        if !self.sessions.contains_key(&username) {
-            let chat_session = self
-                .model
-                .chat();
+        if let Some(chat_sesh) = self.sessions.get_mut(&username) {
+            // existing chat session
+            let output = chat_sesh.add_message(message).await; 
+
+            match output { 
+                Ok(response) => return response, 
+                Err(_) => return String::from("Error! Model couldn't generate response"),
+            }
+        } else { 
+            // new chat session for this user
+            let mut chat_sesh = self.model.chat();
+            let output = chat_sesh.add_message(message).await;
             
-
-            self.sessions.insert(username.clone(), chat_session);
+            match output {
+                Ok(response) => {
+                    self.sessions.insert(username, chat_sesh); 
+                    return response;
+                },
+                Err(_) => return String::from("Error! Model couldn't generate response"),
+            }
         }
-
-        // if it's an existing user, retrieve the session for this user
-        let session = self.sessions.get_mut(&username).unwrap();
-
-        // Send the message
-        let output = session.add_message(message).await;
-
-        if let Ok(response) = output {
-            return response;
-        }
-
-        return String::from("Hello, I am not a bot (yet)!");
     }
 
     #[allow(dead_code)]
-    pub fn get_history(&self, username: String) -> Vec<String> {
-     if let Some(chat) = self.sessions.get(&username) {
+pub fn get_history(&self, username: String) -> Vec<String> {
+    if let Some(chat) = self.sessions.get(&username) {
 
+        // session() returns Result --> must match on it
         if let Ok(session) = chat.session() {
 
+            // history() returns Vec<ChatMessage>
             let history = session.history();
 
-            let mut messages = Vec::new();
+            let mut messages: Vec<String> = Vec::new();
 
+            // iterate through ChatMessage
             for msg in history {
-                messages.push(format!("{:?}", msg));
+                messages.push(msg.content().to_string());
             }
 
+            println!("{:?}", messages); 
             return messages;
         }
     }
-        return Vec::new();
-    }
+
+    Vec::new()
+}
+} 
