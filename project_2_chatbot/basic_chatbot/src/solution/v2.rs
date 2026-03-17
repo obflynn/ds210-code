1f9a7ed89e1cbec845e6be46aec519a41e2916c3
obflynn <obflynn@bu.edu>
2026-03-16 20:44:23 -0400

    chat_with_user function implemented (correctly, I hope)

--- project_2_chatbot/basic_chatbot/src/solution/v2.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v2.rs
@@ -2,29 +2,28 @@ use kalosm::language::*;
 
 #[allow(dead_code)]
 pub struct ChatbotV2 {
-     chat_session: Chat<Llama>,
+     chat_session: Chat<Llama>, // field to store chat session history w/in model
 }
 
 impl ChatbotV2 {
     #[allow(dead_code)]
     pub fn new(model: Llama) -> ChatbotV2 {
         let chat_session = model
-            .chat();
+            .chat(); // creates a new chat session from the model
         
 
         return ChatbotV2 {
-            chat_session
+            chat_session // initialize chatbot struct with chat session data from the model
         };
     }
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, message: String) -> String {
-          // send the message to the bot
-    let output = self.chat_session.add_message(message).await;
+    let output = self.chat_session.add_message(message).await; // store chat session data w/in the chatbot struct 
 
     //get the response if there's no error
     if let Ok(response) = output {
-        return response;
+        return response; 
     }
         return String::from("Hello, I am not a bot (yet)!");
     }
--- project_2_chatbot/basic_chatbot/src/solution/v3.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v3.rs
@@ -3,20 +3,39 @@ use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct ChatbotV3 {
-     model: Llama,
-    sessions: HashMap<String, Chat<Llama>>,
+    model: Llama, 
+    sessions: HashMap<String, Chat<Llama>>, 
 }
 
 impl ChatbotV3 {
     #[allow(dead_code)]
     pub fn new(model: Llama) -> ChatbotV3 {
         return ChatbotV3 {
-
+            model: model, sessions: HashMap::new() // initialize HashMap to store chat session data for each user
         };
     }
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        
+        if let Some(chat_sesh) = self.sessions.get_mut(&username) { // check if the user has an existing chat session (sesh) and retrieve username/session data if it exists
+            let output = chat_sesh.add_message(message).await; // add user's message to chat sesh data
+
+            match output { // respond to user with model output unless there's an error
+                Ok(response) => return response, 
+                Err(_) => return String::from("Error! Model couldn't generate response"),
+            }
+        } 
+        else { // if the user has not messaged the bot before, create a new chat sesh for that user and add their messages to the session data
+            let mut chat_sesh = self.model.chat();
+            let output = chat_sesh.add_message(message).await;
+            
+            match output {
+                Ok(response) => {
+                    self.sessions.insert(username, chat_sesh); // store chat messages in session HashMap -> key = username, value = message data
+                    return response;
+                },
+                Err(_) => return String::from("Error! Model couldn't generate response"),
+            }
+        }
     }
 }
