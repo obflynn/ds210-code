07a76b1a92eead69e5b23cb4d2820f1f6f20d56f
obflynn <obflynn@bu.edu>
2026-03-23 00:36:39 -0400

    remove_least_recently_used implementation

--- project_2_chatbot/cache_chatbot/src/solution/slow_cache.rs
+++ project_2_chatbot/cache_chatbot/src/solution/slow_cache.rs
@@ -32,8 +32,11 @@ impl<V> Cache<V> {
 
     // Helper functions.
     fn remove_least_recently_used(&mut self) {
-        // TODO: your code goes here.
-        // println!("Removing least recently used");
+        let index_of_lru = self.usage_history.len() - 1; // the LRU chat is should be the last element in usage_history
+        let remove_lru = self.usage_history.remove(index_of_lru); // remove the LRU chat from usage_history
+        self.hashmap.remove(&remove_lru); // remove the LRU chat from the cache
+
+        println!("Removing least recently used");
     }
     fn mark_as_most_recently_used(&mut self, username: String) {
         // TODO: your code goes here.
