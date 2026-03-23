09e14a1f0577a667ccae622bad00b9365abf6bc3
obflynn <obflynn@bu.edu>
2026-03-23 19:13:57 -0400

    get_history implementation for v5

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -40,18 +40,20 @@ impl ChatbotV5 {
         let cached_chat = self.cache.get_chat(&username);
 
         match cached_chat {
-            None => {
+            None => { 
                 println!("get_history: {username} is not in the cache!");
-                // TODO: The cache does not have the chat. What should you do?
-                // Your code goes here.
-                return Vec::new();
+                self.cache.insert_chat(username, self.model.chat()); // if the user doesn't have a chat session in the cache, then create one and insert it into the cache
+                return Vec::new(); // if the user doesn't have a chat session in the cache, then they don't have any chat history, so return an empty vector
             }
             Some(chat_session) => {
                 println!("get_history: {username} is in the cache! Nice!");
-                // TODO: The cache has this chat. What should you do?
-                // Your code goes here.
-                return Vec::new();
-
+                let history = chat_session.session().unwrap().history(); // 
+                
+                let mut messages: Vec<String> = Vec::new(); // vector to store each chat message in user's history 
+                for msg_i in history { // iterate through chats in history 
+                    messages.push(msg_i.content().to_string()); // push the content of each chat into the messages vector
+                }
+                return messages; // final returned item is a vector of strings (aka user's chat history)
             }
         }
     }
