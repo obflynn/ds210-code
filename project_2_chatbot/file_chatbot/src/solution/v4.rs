455ce5b1e4d9c331cb061e092599ff4d03a9388e
obflynn <obflynn@bu.edu>
2026-03-23 00:33:52 -0400

    chat_with_user implementation - hopefully works!

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -1,5 +1,5 @@
 use kalosm::language::*;
-use crate::solution::file_library;
+use crate::solution::file_library::{self, save_chat_session_to_file};
 
 pub struct ChatbotV4 {
     model: Llama,
@@ -18,13 +18,24 @@ impl ChatbotV4 {
         let mut chat_session: Chat<Llama> = self.model
             .chat()
             .with_system_prompt("The assistant will act like a pirate");
+        
+        let output = chat_session.add_message(message).await; // model generates response to user message
+        
+        match output { // match statement 1: model responds to user's message unless there's an error
+            Ok(response) => { // match statement 2: if the model is able to generate a response, check if the user has an existing chat session file so the current chat session can be added
+                match file_library::load_chat_session_from_file(&filename) { // check if user has an existing chat session file
+                    Some(session) => { // if the user has a chat session file, save the current chat session to that file
+                        file_library::save_chat_session_to_file(filename, &session); 
+                    }
+                    None => { // report that no chat session file exists for the user
+                        return String::from("No chat history found for user!");
+                    }
+                }
+               return response; // return the model generated response to user
+            },
+            Err(_) => return String::from("Hello, I am not a bot (yet)!"),
+        }
 
-        // TODO: You have to implement the rest:
-        // You need to load the chat session from the file using file_library::load_chat_session_from_file(...).
-        // Think about what needs to happen if the function returns None vs Some(session).
-        // Hint: look at https://docs.rs/kalosm/latest/kalosm/language/struct.Chat.html#method.with_session
-
-        return String::from("Hello, I am not a bot (yet)!");
     }
 
     pub fn get_history(&self, username: String) -> Vec<String> {
