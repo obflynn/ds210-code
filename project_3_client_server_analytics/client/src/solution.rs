8224f722614c406dd95c24ae071c73db7b2f5412
obflynn <obflynn@bu.edu>
2026-04-05 15:32:05 -0400

    run_slow_rpc() implemented

--- project_3_client_server_analytics/client/src/solution.rs
+++ project_3_client_server_analytics/client/src/solution.rs
@@ -9,12 +9,10 @@ pub async fn run_hello(rpc_client: &RPCInterfaceClient) {
 
 pub async fn run_slow_rpc(rpc_client: &RPCInterfaceClient, query: Query) -> Dataset {
     println!("using slow_rpc");
-    
-    // let dataset = rpc_client.slow_rpc().await.unwrap();
-    todo!("Implement this");
-
-    // What should you do to the dataset?
-    // Hint: you have not used `query`, maybe you need to use it somehow?
+    let dataset = rpc_client.slow_rpc(Context::current()).await.unwrap(); // mirrors let result code from run_hello
+    let result = compute_query_on_dataset(&dataset, query); // reference dataset to avoid moving it, query computes result on the dataset and returns it to client as new dataset
+    println!("slow_rpc result: {:?}", result); // print result in terminal to verify it is correct
+    return result; 
 }
 
 pub async fn run_fast_rpc(rpc_client: &RPCInterfaceClient, query: Query) -> Dataset {
