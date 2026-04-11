1c2a6ff1b6b3183fe86fe8175df73cf33f7cfa38
sarudakbu <sarudak@bu.edu>
2026-04-11 11:50:29 -0400

    It passed all the tests, tweaked logic a bit

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -1,86 +1,66 @@
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::{Board, Cell};
+use tic_tac_toe_stencil::board::Board;
 use tic_tac_toe_stencil::player::Player;
 
 // Your solution solution.
-pub struct SolutionAgent {
-    player: Player, // the program
-    //player_opp: Player, // the opponent
-    //board: Board, // the current board state
-}
-//I'm not quite sure what this part is so I'm gonna comment it out first
-/* impl std::ops::Deref for SolutionAgent { 
-    //type Target = Player; 
-
-    //fn deref(&self) -> &Self::Target {  
-       // &self.player
-    //}
-}*/
-    // Board Set-Up:
-    //     0:  1:  2:
-    // 0:  _ | _ | _
-    // 1:  _ | X | _
-    // 2:  _ | _ | _
-    // board.get_cells()[1][1] would return middle cell (aka Cell::X in this case)
-
+pub struct SolutionAgent {}
+  
 
 // Put your solution here.
 impl Agent for SolutionAgent {
         fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
-    // Should returns (<score>, <x>, <y>)
-    // where <score> is your estimate for the score of the game
-    // and <x>, <y> are the position of the move your solution will make.
-
       // add base case if game is over
         if board.game_over() {
             return (board.score(), 0, 0);
         }
 
+        let moves = board.moves(); //We can use board.moves() here instead of looping through the grid cause it already gives all available moves and makes the code cleaner
+//I think the recursion could return incorrect results while this version guarantees correct evaluation of all possible game outcomes
+
 // changed mut score to MIN and Max instead of setting both to 0
 //In minimax, X is maximizing and O is minimizing, so we need to start from the worst possible value to ensure the score gets updated correctly.
 //If we start from 0, we can miss cases where all outcomes are worse than 0 (like all -1 for X), and the algorithm would return the wrong result.
-
-       let mut score = match player {
+let mut best_score = match player {
             Player::X => i32::MIN, // maximize if X
             Player::O => i32::MAX, // minimize if O
         };
 
-       let mut x = 0;
-       let mut y = 0;   
-       let board_cells = board.get_cells();
-       let board_size = board_cells.len();
-       
-       
-       for i in 0..board_size { // iterate through rows
-           
-           for j in 0..board_size { // iterate through columns
-               
-               if board_cells[i][j] == Cell::Empty {// if a cell is empty, then a move can be made there
-                   
-                   let mut copy_board = board.clone();
-                   copy_board.apply_move((i, j), player);
-                   let new_score: i32 = SolutionAgent::solve(&mut copy_board, player, _time_limit).0; 
-                  
-                   if player == Player::X {
-                       if new_score > score { // player X wants to maximize the score
-                           score = new_score; // update score
-                           x = i; // update x so the move is made in the correct row (i)
-                           y = j; // update y so the move is made in the correct column (j)
-                       }
-                   } 
-                   else { // player == Player::O
-                       if new_score < score { // player O wants to minimize the score
-                           score = new_score;
-                           x = i;
-                           y = j;
-                       }
-                   }
-               }
-           }
-       }
-       
-    
-       return (score, x, y); 
+        let mut best_move = moves[0]; // Initializing best_move with the first available move so we always have a valid default in case no better move is found
+        // stores the best position (x, y) that algorithm has found so far
+        // Track best_move separately so we return the correct position associated with the best score
+
+        for (x, y) in moves {
+            // need to clone board so we do not modify the current one
+            let mut new_board = board.clone();
+            new_board.apply_move((x, y), player);
+
+            //Switch player for the recursive call
+            let next_player = match player {
+                Player::X => Player::O,
+                Player::O => Player::X,
+            };
+
+            let (score, _, _) = SolutionAgent::solve(&mut new_board, next_player, _time_limit); //Call solve recursively to evaluate the outcome of this move
+
+            // If the current player is X (maximizing), choose the move with the highest score
+            match player {
+                Player::X => {
+                    if score > best_score {
+                        best_score = score;
+                        best_move = (x, y);
+                    }
+                } // If the current player is O (minimizing), choose the move with the lowest score
+                Player::O => {
+                    if score < best_score {
+                        best_score = score;
+                        best_move = (x, y);
+                    }
+                }
+            }
+        }
+
+        (best_score, best_move.0, best_move.1) //return the best score and the coordinates of the best move
     }
 }
-
+ 
+  
