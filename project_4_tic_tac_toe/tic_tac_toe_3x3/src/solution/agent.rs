775296854d187df24d76aba3e07c5c07733760d9
obflynn <obflynn@bu.edu>
2026-04-10 23:20:33 -0400

    1st attempt at Agent implementation - location that agent picks appears random

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -1,18 +1,76 @@
 use tic_tac_toe_stencil::agents::Agent;
-use tic_tac_toe_stencil::board::Board;
+use tic_tac_toe_stencil::board::{self, Board};
 use tic_tac_toe_stencil::player::Player;
 
 // Your solution solution.
-pub struct SolutionAgent {}
+pub struct SolutionAgent {
+    player: Player, // the program
+    //player_opp: Player, // the opponent
+    //board: Board, // the current board state
+}
+
+impl std::ops::Deref for SolutionAgent { 
+    type Target = Player; 
+
+    fn deref(&self) -> &Self::Target {  
+        &self.player
+    }
+}
+    // Board Set-Up:
+    //     0:  1:  2:
+    // 0:  _ | _ | _
+    // 1:  _ | X | _
+    // 2:  _ | _ | _
+    // board.get_cells()[1][1] would return middle cell (aka Cell::X in this case)
+
 
 // Put your solution here.
 impl Agent for SolutionAgent {
     // Should returns (<score>, <x>, <y>)
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
+
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
         // If you want to make a recursive call to this solution, use
-        // `SolutionAgent::solve(...)`
-        unimplemented!("Not yet implemented")
+        // `SolutionAgent::solve(board, player, _time_limit)
+
+       let mut score = 0;
+       let mut x = 0;
+       let mut y = 0;   
+       let board_cells = board.get_cells();
+       let board_size = board_cells.len();
+       
+       
+       for i in 0..board_size { // iterate through rows
+           
+           for j in 0..board_size { // iterate through columns
+               
+               if board_cells[i][j] == board::Cell::Empty { // if a cell is empty, then a move can be made there
+                   
+                   let mut copy_board = board.clone();
+                   copy_board.apply_move((i, j), player);
+                   let new_score: i32 = SolutionAgent::solve(&mut copy_board, player, _time_limit).0; 
+                  
+                   if player == Player::X {
+                       if new_score > score { // player X wants to maximize the score
+                           score = new_score; // update score
+                           x = i; // update x so the move is made in the correct row (i)
+                           y = j; // update y so the move is made in the correct column (j)
+                       }
+                   } 
+                   else { // player == Player::O
+                       if new_score < score { // player O wants to minimize the score
+                           score = new_score;
+                           x = i;
+                           y = j;
+                       }
+                   }
+               }
+           }
+       }
+       
+    
+       return (score, x, y); 
     }
 }
+
